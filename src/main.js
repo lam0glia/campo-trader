90817130d704921ab937b77af95e0d7eab9527b9
lam0glia <gabslamoglia@outlook.com>
2020-10-08 23:55:42 -0300

    install and import vuesax

--- src/main.js
+++ src/main.js
@@ -1,7 +1,13 @@
 import Vue from 'vue'
+import Vuesax from 'vuesax'
 import App from './App.vue'
 
+// import Vuesax components
+import 'vuesax/dist/vuesax.css' //Vuesax styles
+
 Vue.config.productionTip = false
+
+Vue.use(Vuesax)
 
 new Vue({
   render: h => h(App),
